eafbb783221cade34b003af3d680046fc23561b5
mgruben <GrubenM@gmail.com>
2016-11-14 10:59:06 -0600

    Remove unused helper method

--- PandigitalPrimes.java
+++ PandigitalPrimes.java
@@ -42,20 +42,6 @@ public class PandigitalPrimes {
     }
     
     /**
-     * Is the given string of digits pandigital?
-     * @param s
-     * @return 
-     */
-    private boolean isPandigital(String s) {
-        char[] ch = s.toCharArray();
-        Arrays.sort(ch);
-        for (int i = 0; i < ch.length; i++) {
-            if (ch[i] != i+1 + '0') return false;
-        }
-        return true;
-    }
-    
-    /**
      * Given an integer array representing a decimal integer,
      * returns that decimal integer.
      * 
