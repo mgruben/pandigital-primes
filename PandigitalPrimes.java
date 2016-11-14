087d02b386e265797e0141b1f3677ea7e195c905
mgruben <GrubenM@gmail.com>
2016-11-14 11:31:52 -0600

    Docstring more consistently

--- PandigitalPrimes.java
+++ PandigitalPrimes.java
@@ -80,16 +80,21 @@ public class PandigitalPrimes {
         a[l] = tmp;
     }
     
+    /**
+     * Permute the array of digits in-place to the next-largest permutation.
+     * No change is made if the array is already the smallest permutation.
+     * @param a 
+     */
     public void permute(int[] a) {
         /**
-         * permute the array of digits in largest-to-smallest order
-        Find the largest index k such that a[k] > a[k + 1]. If no such index exists, the permutation is the last permutation.
-        Find the largest index l greater than k such that a[k] > a[l].
-        Swap the value of a[k] with that of a[l].
-        Reverse the sequence from a[k + 1] up to and including the final element a[n].
-        * 
-        * https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
-        */
+         * from https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+         * 
+         * (1) Find the largest index k such that a[k] > a[k + 1].
+         *    If no such index exists, the permutation is the last permutation.
+         * (2) Find the largest index l greater than k such that a[k] > a[l].
+         * (3) Swap the value of a[k] with that of a[l].
+         * (4) Reverse the sequence from a[k + 1] up to and including the final element a[n].
+         */
         
         // Find the largest index k
         int k;
